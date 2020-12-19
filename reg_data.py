d76cbaea5822017362180cc8e3e7adcacdc68994
bdelarosa <bladegt@msn.com>
2020-12-19 02:07:07 -0400

    final

--- reg_data.py
+++ reg_data.py
@@ -2,37 +2,113 @@ import sys
 import os
 
 
-def register_data(file):
-    doc_dir = os.path.join(os.getcwd(), file)
+def main():
+    doc_dir = os.path.join(os.getcwd(), sys.argv[1])
+    lista = []
+    option = 0
+
+    while option == 0:
+        selected = input('''
+Elige la opción deseada:
+[1] Agregar
+[2] Listar
+[3] Buscar
+[4] Salir
+
+''')
+
+        if selected == '1':
+            register_data(doc_dir)
+
+        if selected == '2':
+            list_data(doc_dir)
+
+        if selected == '3':
+            find_data(doc_dir)
+
+        if selected == '4':
+            print('Adios!')
+            option += 1
+
+
+def register_data(doc_dir):
     header = 'Cedula,Nombre,Apellido,Edad'
     counter = 0
 
-    with open(doc_dir, 'a+') as doc:
-        if os.path.getsize(doc_dir) == 0:
-            doc.write(header)
+    try:
+        with open(doc_dir, 'a+') as doc:
+            if os.path.getsize(doc_dir) == 0:
+                doc.write(header)
 
-        while counter == 0:
-            print('Ingresa tus datos.')
-            cid = input('Cedula: ')
-            name = input('Nombre: ')
-            lastname = input('Apellido: ')
-            age = input('Edad: ')
-            data = f'{cid},{name},{lastname},{age}'
+            while counter == 0:
+                print('Ingresa tus datos.')
+                cid = input('Cedula: ')
+                name = input('Nombre: ')
+                lastname = input('Apellido: ')
+                age = input('Edad: ')
+                data = f'{cid},{name},{lastname},{age}'
 
-            respuesta = input('¿Deseas guardar? si/no\n')
+                respuesta = input('¿Deseas guardar? si/no\n')
 
-            if respuesta == 'no':
-                counter += 1
+                if respuesta == 'no':
+                    counter += 1
 
+                else:
+                    doc.write('\n')
+                    doc.write(data)
+    except IOError:
+        print('Ruta del documento inválida.')
+
+
+def list_data(doc_dir):
+    try:
+        with open(doc_dir, 'r') as doc:
+            lines = doc.readlines()
+            if len(lines) < 2:
+                print('No hay registros.')
             else:
-                doc.write('\n')
-                doc.write(data)
+                doc.seek(0)
+                head = doc.readline().strip('\n').split(',')
+                for idx, line in enumerate(lines, 1):
+                    split_line = line.strip('\n').split(',')
+                    print(f'''
+    Record #{idx}:
+    {head[0]}: {split_line[0]}
+    {head[1]}: {split_line[1]}
+    {head[2]}: {split_line[2]}
+    {head[3]}: {split_line[3]}
+    ''')
+    except IOError:
+        print('Ruta del documento inválida.')
 
-        print('Adios')
+
+def find_data(doc_dir):
+    cid = input('Cedula: ')
+    try:
+        with open(doc_dir, 'r') as doc:
+            lines = doc.readlines()
+            if len(lines) < 2:
+                print('No hay registros que buscar.')
+            else:
+                doc.seek(0)
+                head = doc.readline().strip('\n').split(',')
+                for idx, line in enumerate(lines, 1):
+                    split_line = line.strip('\n').split(',')
+
+                    if split_line[0] == cid:
+                        print(f'''
+    Record #{idx}:
+    {head[0]}: {split_line[0]}
+    {head[1]}: {split_line[1]}
+    {head[2]}: {split_line[2]}
+    {head[3]}: {split_line[3]}
+    ''')
+
+    except IOError:
+        print('Ruta del documento inválida.')
 
 
-if __name__ == '__main__':
-    if len(sys.argv) >= 2:
-        register_data(sys.argv[1])
-    else:
-        print('Especificar document. ej. reg_data.py datos.csv')
+if len(sys.argv) >= 2:
+    main()
+else:
+    print('Especificar document. ej. reg_data.py datos.csv')
